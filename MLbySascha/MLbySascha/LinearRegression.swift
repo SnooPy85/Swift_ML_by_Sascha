dcc281b4559fbdfc34459636b768351f25f706a8
Sascha <48602864+SnooPy85@users.noreply.github.com>
2021-02-23 21:18:32 +0100

    initial commit

---
+++ MLbySascha/MLbySascha/LinearRegression.swift
@@ -0,0 +1,12 @@
+//
+//  LinearRegression.swift
+//  MLbySascha
+//
+//  Created by Sascha Rexhäuser on 23.02.21.
+//
+
+import Foundation
+
+class LinearRegression {
+    
+}
---
+++ MLbySascha/MLbySascha/MLbySascha.swift
@@ -0,0 +1,10 @@
+//
+//  MLbySascha.swift
+//  MLbySascha
+//
+//  Created by Sascha Rexhäuser on 23.02.21.
+//
+
+class MLbySascha {
+
+}
